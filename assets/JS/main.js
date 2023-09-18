a4935a197120b30712cbc5039d7b0d0c437ad065
frontendfantasy <141944140+frontendfantasy@users.noreply.github.com>
2023-09-18 16:34:48 +0500

    Edited JavaScript & Css

---
+++ assets/JS/main.js
@@ -0,0 +1,9 @@
+var totalAge = (90 - age);
+
+    var totalDays = (totalAge * 365);
+
+    var totalWeeks = (totalAge * 52);
+
+    var totalMonths = (totalAge * 12);
+
+    console.log("You have " +totalDays+ " days, "  +totalWeeks + " weeks, and " +totalMonths+ " months left.");
